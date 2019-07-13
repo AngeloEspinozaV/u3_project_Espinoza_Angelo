2bef935270d0a13c40414f5696d6c78775385a46
Angelo Espinoza <al1609046@upy.edu.mx>
2019-07-13 01:11:01 -0500

    Created functions manual and autonomous

--- controllers/u3_project_Espinoza_Angelo/u3_project_Espinoza_Angelo.c
+++ controllers/u3_project_Espinoza_Angelo/u3_project_Espinoza_Angelo.c
@@ -130,45 +130,6 @@ int main(int argc, char **argv)
       printf("Position sensor wheel 2 is: %.4f\n", position_sensor_value2);
       printf("Position sensor wheel 3 is: %.4f\n", position_sensor_value3);
 
-      /* MOVE FORWARD */
-      // if ((distance_sensor_value1 > desired_centimeters) && (distance_sensor_value2 > desired_centimeters)) {
-
-         // wb_motor_set_velocity(motor_1, 1);
-         // wb_motor_set_velocity(motor_2, 1);
-         // wb_motor_set_velocity(motor_3, 0);
-      // }
-
-      // /* STOP */
-      // else if ((distance_sensor_value1 == desired_centimeters) && (distance_sensor_value2 == desired_centimeters)) {
-
-         // wb_motor_set_velocity(motor_1, 0);
-         // wb_motor_set_velocity(motor_2, 0);
-         // wb_motor_set_velocity(motor_3, 0);
-      // }
-
-      // /* MOVE RIGHT AROUND A PIVOT */
-      // else if((distance_sensor_value1 < desired_centimeters) && (distance_sensor_value2 < desired_centimeters)) {
-
-         // wb_motor_set_velocity(motor_1, 0);
-         // wb_motor_set_velocity(motor_2, -2);
-         // wb_motor_set_velocity(motor_3, -2);
-      // }
-
-      // /* AVOID OBSTACLES LEFT */
-      // else if(distance_sensor_value2 <= desired_centimeters) {
-
-         // wb_motor_set_velocity(motor_1, 0);
-         // wb_motor_set_velocity(motor_2, 3);
-         // wb_motor_set_velocity(motor_3, 3);
-      // }
-      // /* AVOID OBSTACLES RIGHT */
-      // else if(distance_sensor_value1 <= desired_centimeters) {
-
-         // wb_motor_set_velocity(motor_1, 3);
-         // wb_motor_set_velocity(motor_2, 0);
-         // wb_motor_set_velocity(motor_3, -3);
-      // }
-
   };
 
   wb_robot_cleanup();
